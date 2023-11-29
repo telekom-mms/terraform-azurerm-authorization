9a5dee46f90db2d8150cdcef5b7c501715159d36
Hauke Brandt <hauke.brandt@telekom.de>
2023-11-29 15:18:40 +0100

    Module now contains a resource for managing user assigned identities

--- outputs.tf
+++ outputs.tf
@@ -9,6 +9,17 @@ output "role_assignments" {
   }
 }
 
+output "user_assigned_identity" {
+  description = "Outputs all attributes of resource_type."
+  value = {
+    for user_assigned_identity in keys(azurerm_user_assigned_identity.user_assigned_identity) :
+    user_assigned_identity => {
+      for key, value in azurerm_user_assigned_identity.user_assigned_identity[user_assigned_identity] :
+      key => value
+    }
+  }
+}
+
 output "variables" {
   description = "Displays all configurable variables passed by the module. __default__ = predefined values per module. __merged__ = result of merging the default values and custom values passed to the module"
   value = {
@@ -20,6 +31,10 @@ output "variables" {
       role_assignment = {
         for key in keys(var.role_assignment) :
         key => local.role_assignment[key]
+      }
+      user_assigned_identity = {
+        for key in keys(var.user_assigned_identity) :
+        key => local.user_assigned_identity[key]
       }
     }
   }
